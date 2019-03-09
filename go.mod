23f62f9b95c3dee133051208d0adc14531bc428c
Mario Finelli <mario@finel.li>
2019-03-09 12:13:33 -0500

    Add help and version options

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mfinelli/musicrename
 
 go 1.12
+
+require github.com/pborman/getopt v0.0.0-20190308214855-fd6d657c3083 // indirect
--- mr.go
+++ mr.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+import "os"
+import "github.com/pborman/getopt"
+
+const VERSION = "0.0.1"
+
 func main() {
-    return
+	versionFlag := getopt.BoolLong("version", 'v', "print version")
+	helpFlag := getopt.BoolLong("help", 'h', "print help")
+	getopt.Parse()
+
+	if *helpFlag {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	if *versionFlag {
+		fmt.Printf("musicrename v%s\n", VERSION)
+		os.Exit(0)
+	}
 }
