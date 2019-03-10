7db50e857530fb2a90550d8f55e9d648874c16f9
Mario Finelli <mario@finel.li>
2019-03-10 18:58:36 -0400

    Make sanitize more robust

--- util/sanitize.go
+++ util/sanitize.go
@@ -1,16 +1,62 @@
 package util
 
+import "regexp"
+import "strings"
 import "unicode"
 import "golang.org/x/text/transform"
 import "golang.org/x/text/unicode/norm"
 import "golang.org/x/text/runes"
 
 func Sanitize(str string, max int) string {
-	return translit(str)
+	return lastChar(unrecognized(whitespace(colons(maxlen(translit(str), max)))))
 }
 
 func translit(str string) string {
 	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
 	s, _, _ := transform.String(t, str)
 	return s
+}
+
+func maxlen(str string, max int) string {
+	if len(str) > max {
+		return str[0:max]
+	}
+
+	return str
+}
+
+func lastChar(str string) string {
+	notAllowed := []rune{'(', '.', '-', '[', ',', '&', ' '}
+	runes := []rune(str)
+
+	for contains(runes[len(runes)-1], notAllowed) {
+		runes = runes[:len(runes)-1]
+	}
+
+	return strings.TrimSpace(string(runes))
+}
+
+func contains(needle rune, haystack []rune) bool {
+	for _, hay := range haystack {
+		if needle == hay {
+			return true
+		}
+	}
+	return false
+}
+
+func colons(str string) string {
+	re := regexp.MustCompile("(-\\s+)")
+	return re.ReplaceAllString(str, " ")
+}
+
+func whitespace(str string) string {
+	// consolidate whitespace
+	re := regexp.MustCompile("(\\s+)")
+	return re.ReplaceAllString(str, " ")
+}
+
+func unrecognized(str string) string {
+	re := regexp.MustCompile("([^0-9A-Za-z,& \\-\\(\\)\\[\\]\\.])")
+	return re.ReplaceAllString(str, "")
 }
--- util/sanitize_test.go
+++ util/sanitize_test.go
@@ -11,10 +11,17 @@ func TestSanitize(t *testing.T) {
 	}{
 		{"test", "test"},
 		{"tèst", "test"},
+		{"waytoomanycharacters", "waytoomany"},
+		{"remove -", "remove"},
+		{"a - [", "a"},
+		{"a- b", "a b"},
+		{"a- b- c", "a b c"},
+		{"a   c", "a c"},
+		{"a#d", "ad"},
 	}
 
 	for _, test := range tests {
-		str := util.Sanitize(test.input)
+		str := util.Sanitize(test.input, 10)
 		if str != test.output {
 			t.Errorf("expected %s but got %s", test.output, str)
 		}
