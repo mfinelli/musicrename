bd861332e18cadcc107c5f352f1a3fe70fb710fa
Mario Finelli <mario@finel.li>
2019-03-10 15:11:03 -0400

    Move artist directories

--- walk/walk.go
+++ walk/walk.go
@@ -3,6 +3,7 @@ package walk
 import "fmt"
 import "io/ioutil"
 import "os"
+import "path"
 
 import "github.com/gookit/color"
 
@@ -22,8 +23,32 @@ func WalkAndProcessDirectory(verbose bool, dry bool, dir string) [2]int {
 		if artist.IsDir() {
 			dirCount += 1
 			util.Printf(fmt.Sprintf("Found artist: %s\n", artist.Name()), color.Cyan)
+			handleArtistDir(verbose, dry, dir, artist.Name())
 		}
 	}
 
 	return [2]int{dirCount, fileCount}
+}
+
+func handleArtistDir(verbose bool, dry bool, workdir string, dir string) string {
+	sanitized := util.Sanitize(dir)
+
+	if sanitized != dir {
+		if verbose {
+			util.Printf(fmt.Sprintf("Rename %s to %s\n", dir, sanitized), color.Yellow)
+		}
+
+		if !dry {
+			err := os.Rename(path.Join(workdir, dir), path.Join(workdir, sanitized))
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			return sanitized
+		}
+	}
+
+	return dir
 }
