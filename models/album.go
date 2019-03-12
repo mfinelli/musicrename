992b73f57c0b368d21feab2436aee630d9c5905a
Mario Finelli <mario@finel.li>
2019-03-12 18:18:20 -0400

    Add function to parse album from string

--- models/album.go
+++ models/album.go
@@ -1,7 +1,10 @@
 package models
 
+import "errors"
 import "fmt"
 import "path"
+import "regexp"
+import "strconv"
 
 type Album struct {
 	Artist *Artist
@@ -16,4 +19,23 @@ func (a *Album) String() string {
 
 func (a *Album) FullPath() string {
 	return path.Join(a.Artist.FullPath(), a.String())
+}
+
+func ParseAlbum(dir string) (Album, error) {
+	if m, _ := regexp.MatchString("^\\[\\d{4}\\] .*$", dir); m {
+		yearStr := dir[1:5]
+		title := dir[7:len(dir)]
+		year, err := strconv.Atoi(yearStr)
+
+		if err != nil {
+			return Album{}, err
+		}
+
+		return Album{
+			Year: year,
+			Name: title,
+		}, nil
+	}
+
+	return Album{}, errors.New(fmt.Sprintf("Unable to parse album from: %s", dir))
 }
--- models/album_test.go
+++ models/album_test.go
@@ -1,5 +1,6 @@
 package models_test
 
+import "reflect"
 import "testing"
 import "github.com/mfinelli/musicrename/models"
 
@@ -31,6 +32,23 @@ func TestAlbumFullPath(t *testing.T) {
 	for _, test := range tests {
 		if test.a.FullPath() != test.exp {
 			t.Errorf("Expected %s but got %s", test.exp, test.a.FullPath())
+		}
+	}
+}
+
+func TestParseAlbum(t *testing.T) {
+	tests := []struct {
+		input string
+		album models.Album
+	}{
+		{"[2000] test album", models.Album{Year: 2000, Name: "test album"}},
+		{"notanalbum", models.Album{}},
+	}
+
+	for _, test := range tests {
+		a, _ := models.ParseAlbum(test.input)
+		if !reflect.DeepEqual(a, test.album) {
+			t.Errorf("Expected %v but got %v", test.album, a)
 		}
 	}
 }
