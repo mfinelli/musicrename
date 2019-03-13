1096457d376ec0d76c057d5871e0eab7a11e2388
Mario Finelli <mario@finel.li>
2019-03-13 17:49:32 -0400

    Add parse and fullpath functions for extras

--- models/extra.go
+++ models/extra.go
@@ -1,6 +1,7 @@
 package models
 
 import "fmt"
+import "path"
 
 type Extra struct {
 	ExtraDir *ExtraDir
@@ -11,4 +12,19 @@ type Extra struct {
 
 func (e *Extra) String() string {
 	return fmt.Sprintf("%s.%s", e.Name, e.Format)
+}
+
+func (e *Extra) FullPath() string {
+	return path.Join(e.ExtraDir.FullPath(), e.RealPath)
+}
+
+func ParseExtra(item string) (Extra, error) {
+	ext := path.Ext(item)
+	name := item[0 : len(item)-len(ext)]
+
+	return Extra{
+		RealPath: item,
+		Name:     name,
+		Format:   ext[1:len(ext)],
+	}, nil
 }
--- models/extra_test.go
+++ models/extra_test.go
@@ -1,5 +1,6 @@
 package models_test
 
+import "reflect"
 import "testing"
 
 import "github.com/mfinelli/musicrename/models"
@@ -15,6 +16,43 @@ func TestExtraString(t *testing.T) {
 	for _, test := range tests {
 		if test.e.String() != test.exp {
 			t.Errorf("Expected %s but got %s", test.exp, test.e.String())
+		}
+	}
+}
+
+func TestExtraFullPath(t *testing.T) {
+	artist := models.Artist{RootDir: "/tmp", RealPath: "test", Name: "test"}
+	album, _ := models.ParseAlbum("[2000] test album")
+	artist.AddAlbum(&album)
+	extradir, _ := models.ParseExtraDir("test")
+	album.AddExtraDir(&extradir)
+
+	tests := []struct {
+		e   models.Extra
+		exp string
+	}{
+		{models.Extra{ExtraDir: &extradir, RealPath: "test.png", Name: "test", Format: "png"}, "/tmp/test/[2000] test album/test/test.png"},
+	}
+
+	for _, test := range tests {
+		if test.e.FullPath() != test.exp {
+			t.Errorf("Expected %s but got %s", test.exp, test.e.FullPath())
+		}
+	}
+}
+
+func TestParseExtra(t *testing.T) {
+	tests := []struct {
+		input string
+		extra models.Extra
+	}{
+		{"test.png", models.Extra{RealPath: "test.png", Name: "test", Format: "png"}},
+	}
+
+	for _, test := range tests {
+		e, _ := models.ParseExtra(test.input)
+		if !reflect.DeepEqual(e, test.extra) {
+			t.Errorf("Expected %v but got %v", test.extra, e)
 		}
 	}
 }
