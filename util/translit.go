be768572bf683593576e23f653a3f1c24cf9f7af
Mario Finelli <mario@finel.li>
2026-01-02 17:21:59 -0500

    Re-introduce translit function and add tests

---
+++ util/translit.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2020-2026 Mario Finelli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package util
+
+import (
+	"github.com/alexsergivan/transliterator"
+)
+
+func SanitizePathSegment(str string) (string, error) {
+	t := translit(str)
+	return t, nil
+}
+
+func translit(str string) string {
+	t := transliterator.NewTransliterator(nil)
+	return t.Transliterate(str, "en")
+}
