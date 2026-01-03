f008892f6a6b42260f963e2df9a34c5f26bcf1a4
Mario Finelli <mario@finel.li>
2026-01-02 21:48:42 -0500

    Fix filepaths

--- walk/contents.go
+++ walk/contents.go
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2026 Mario Finelli
+ * Copyright © 2026 Mario Finelli
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
